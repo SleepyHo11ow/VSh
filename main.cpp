c641954289107a4e6f9f4a5a2f84a0325265b117
SleepyHo11ow <119799839+SleepyHo11ow@users.noreply.github.com>
2022-12-04 22:05:06 +0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,14 +2,27 @@
 
 using namespace std;
 double my_pow(double num , unsigned int step){
-    for(int i = 0; i<step; i++)
-        num*=num;
+
+    {
+           if (step == 0)
+
+           {
+                   return 1;
+
+           }
+           if (step % 2 == 0)
+           {
+                   return my_pow(num * num, step / 2);
+           }
+           return num * my_pow(num, step - 1);
+
+    }
 }
 
 int main()
 {
    unsigned int step = 2;
    double num = 2.33 ;
-   my_pow(num, step);
+  double r = my_pow(num, step);
     return 0;
 }
